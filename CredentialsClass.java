4bdfd4217392c2c008ed6fd33f13ea17a95ebaaf
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2021-06-08 23:39:37 +0530

    Delete CredentialsClass.java

--- CredentialsClass.java
+++
@@ -1,7 +0,0 @@
-package BusinessGenie.app.Bossex;
-
-public class CredentialsClass {
-    //user="BossexClient";
-    public static  String user="BusinessBuddyClient";
-    public static  String password="businessGenie9@gmail.com";
-}
